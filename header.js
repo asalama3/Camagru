2790a233a6f2cf1fd25dcdf91e19a96cea29b9f9
Andrea Salama <andrea@Andreas-Air.home>
2016-10-12 23:08:51 -0400

    header js file

---
+++ header.js
@@ -0,0 +1,11 @@
+function menu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        x.style.overflow="hidden";
+    } else {
+        x.className = "topnav";
+        x.style.overflow="visible";
+
+    }
+}
